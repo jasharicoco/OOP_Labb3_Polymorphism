976c6883fbc94083c220ab661fba90a2490c4c12
jasharicoco <jashari.egzon@hotmail.com>
2024-10-29 19:21:30 +0100

    Add project files.

---
+++ OOP_Labb3_Polymorphism/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OOP_Labb3_Polymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
